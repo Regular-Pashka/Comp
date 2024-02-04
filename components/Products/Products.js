ff952b8a0e6b3438ae9d4cdf0b2a6f8568643b13
Wismat <paxa39ru@gmail.com>
2024-02-04 12:35:15 +0200

    finished markup

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,46 +1,56 @@
 class Products {
-    constructor() {
-        this.classNameActive = 'products-element__btn_active';
-        this.labelAdd = 'Add to cart';
-        this.labelRemove = 'Remove from cart';
-    }
+	constructor() {
+		this.classNameActive = 'products-element__btn_active';
+		this.labelAdd = 'Add to cart';
+		this.labelRemove = 'Remove from cart';
+	}
 
-    handleSetLocationStorage() {
-        console.log('ok');
-    }
+	handleSetLocationStorage(element, id) {
+		const { pushProduct, products } = localStorageUtil.putProducts(id);
 
-    render() {
-        const productsStore = localStorageUtil.getProducts();
-        let htmlCatalog = "";
+		if (pushProduct) {
+			element.classList.add(this.classNameActive);
+			element.innerHTML = this.labelRemove;
+		} else {
+			element.classList.remove(this.classNameActive);
+			element.innerHTML = this.labelAdd;
+		}
 
-        CATALOG.forEach(({ id, name, price, img }) => {
-            let activeClass = '';
-            let activeText = '';
+		headerPage.render(products.length);
+	}
 
-            if (productsStore.indexOf(id) === -1) {
-                activeText = this.labelAdd
-            } else {
-                activeClass = ' ' + this.classNameActive;
-                activeText = this.labelRemove;
-            }
-            htmlCatalog += `
-                <li class="products-element">
-                    <span class="products-element__name">${name}</span>
-                    <img class="products-element__img" src="${img}" />
-                    <span class="products-element__price"> ${price.toLocaleString()} USD </span>
-                    <button class="products-element__btn${activeClass}" onClick="productsPage.handleSetLocationStorage()">
-                    ${activeText}
-                    </button>
-                </li>
-            `;
-        })
-        const html = `
-            <ul class="products-container">
-                ${htmlCatalog}
-            </ul>
-        `;
-        ROOT_PRODUCTS.innerHTML = html;
-    }
+	render() {
+		const productsStore = localStorageUtil.getProducts();
+		let htmlCatalog = "";
+
+		CATALOG.forEach(({ id, name, price, img }) => {
+			let activeClass = '';
+			let activeText = '';
+
+			if (productsStore.indexOf(id) === -1) {
+					activeText = this.labelAdd
+			} else {
+					activeClass = ' ' + this.classNameActive;
+					activeText = this.labelRemove;
+			}
+			htmlCatalog += `
+					<li class="products-element">
+						<span class="products-element__name">${name}</span>
+						<img class="products-element__img" src="${img}" />
+						<span class="products-element__price"> ${price.toLocaleString()} USD </span>
+						<button class="products-element__btn${activeClass}" onClick="productsPage.handleSetLocationStorage(this, '${id}')">
+						${activeText}
+						</button>
+					</li>
+			`;
+		})
+		const html = `
+			<ul class="products-container">
+					${htmlCatalog}
+			</ul>
+		`;
+		ROOT_PRODUCTS.innerHTML = html;
+	}
 }
 const productsPage = new Products();
 productsPage.render();
